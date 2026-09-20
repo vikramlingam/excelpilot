e543a5d7c8bddf01587f49fababf62001b3d3f2c
Vikram Lingam <aarvi.2705@gmail.com>
2026-09-20 12:34:24 +0530

    Pass trusted office-addin-dev-certs SSL options to webpack dev-server

--- addin/webpack.config.js
+++ addin/webpack.config.js
@@ -1,70 +1,87 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const devCerts = require("office-addin-dev-certs");
 
-module.exports = {
-  entry: {
-    taskpane: "./src/taskpane/index.tsx",
-    commands: "./src/commands/commands.ts",
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
-    clean: true,
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".html", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|ico)$/,
-        type: "asset/resource",
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "taskpane.html",
-      template: "./src/taskpane/taskpane.html",
-      chunks: ["taskpane"],
-    }),
-    new HtmlWebpackPlugin({
-      filename: "commands.html",
-      template: "./src/commands/commands.html",
-      chunks: ["commands"],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
+async function getHttpsOptions() {
+  const httpsOptions = await devCerts.getHttpsServerOptions();
+  return {
+    ca: httpsOptions.ca,
+    key: httpsOptions.key,
+    cert: httpsOptions.cert,
+  };
+}
+
+module.exports = async () => {
+  const httpsOptions = await getHttpsOptions();
+
+  return {
+    entry: {
+      taskpane: "./src/taskpane/index.tsx",
+      commands: "./src/commands/commands.ts",
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].bundle.js",
+      clean: true,
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".html", ".js"],
+    },
+    module: {
+      rules: [
         {
-          from: "assets/*",
-          to: "assets/[name][ext]",
-          noErrorOnMissing: true,
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
         },
         {
-          from: "manifest.xml",
-          to: "manifest.xml",
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|ico)$/,
+          type: "asset/resource",
         },
       ],
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
     },
-    headers: {
-      "Access-Control-Allow-Origin": "*",
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: "taskpane.html",
+        template: "./src/taskpane/taskpane.html",
+        chunks: ["taskpane"],
+      }),
+      new HtmlWebpackPlugin({
+        filename: "commands.html",
+        template: "./src/commands/commands.html",
+        chunks: ["commands"],
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "assets/*",
+            to: "assets/[name][ext]",
+            noErrorOnMissing: true,
+          },
+          {
+            from: "manifest.xml",
+            to: "manifest.xml",
+          },
+        ],
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "dist"),
+      },
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      server: {
+        type: "https",
+        options: httpsOptions,
+      },
+      port: 3000,
     },
-    server: "https",
-    port: 3000,
-  },
+  };
 };
